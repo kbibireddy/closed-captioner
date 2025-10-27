ac3513c3fa2cf72c79e90553b9933a8e0e53c722
Karthik Bibireddy <karthikbibireddy@hotmail.com>
2025-10-26 21:58:05 -0700

    Initial Commit

---
+++ ClosedCaptioner/ClosedCaptionerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClosedCaptionerApp.swift
+//  ClosedCaptioner
+//
+//  Created by Karthik Bibireddy on 10/26/25.
+//
+
+import SwiftUI
+
+@main
+struct ClosedCaptionerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClosedCaptioner/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ClosedCaptioner
+//
+//  Created by Karthik Bibireddy on 10/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
